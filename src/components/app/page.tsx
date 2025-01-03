a4ebced39f35634a9178c4137657ad4e35f296c1
Jacob Pusheck <jpusheck@apple.com>
2025-01-02 17:29:01 -0800

    Init
    Init

---
+++ src/components/app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import RetirementDashboard from '../components/RetirementDashboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <RetirementDashboard />
+    </main>
+  );
+}
